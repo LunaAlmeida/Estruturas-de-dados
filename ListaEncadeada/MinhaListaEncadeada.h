9c632635a32af59fea5edcbae98267ae00328050
Luna dos Santos Almeida <62681460+LunaAlmeida@users.noreply.github.com>
2021-11-18 16:27:57 -0400

    ListaCorreta

--- ListaEncadeada/MinhaListaEncadeada.h
+++ ListaEncadeada/MinhaListaEncadeada.h
@@ -2,7 +2,6 @@
 #define MINHALISTAENCADEADA_H
 
 #include "ListaEncadeadaAbstrata.h"
-#include <stdlib.h>
 
 template <typename T>
 class MinhaListaEncadeada :  public ListaEncadeadaAbstrata<T>
@@ -10,30 +9,26 @@ class MinhaListaEncadeada :  public ListaEncadeadaAbstrata<T>
     //Implemente aqui as funcões marcadas com virtual na ListaEncadeadaAbstrata
     //Lembre-se de implementar o construtor e destrutor da classe
 
-private:
-    Elemento<T>* dadoPosicao = new Elemento<T>;
-    Elemento<T>* dadoContem = new Elemento<T>;
-    Elemento<T>* novoDado_inicio = new Elemento<T>;
-    Elemento<T>* novoDado_posicao = new Elemento<T>;
-    Elemento<T>* dadoAnterior = new Elemento<T>;
-    Elemento<T>* dadoRetirar = new Elemento<T>;
-    Elemento<T>* dadoProximo = new Elemento<T>;
 public:
     MinhaListaEncadeada() 
     {
         ListaEncadeadaAbstrata<T>::_primeiro = NULL;
         ListaEncadeadaAbstrata<T>::_tamanho = 0;
     };
-
+    
     ~MinhaListaEncadeada()
     {
-        /*delete novoDado_inicio;
-        delete dadoContem;
-        delete dadoPosicao;
-        delete novoDado_posicao;
-        delete dadoAnterior;
-        delete dadoRetirar;
-        delete dadoProximo;*/
+        Elemento<T>* Lista;
+        Elemento<T>* primeiro;
+        
+        Lista = ListaEncadeadaAbstrata<T>::_primeiro;
+        while(Lista != NULL)
+        {
+            primeiro = Lista;
+            Lista = Lista->_proximo;
+            delete primeiro;
+        }
+        
     };
 
     int getTamanho() {
@@ -49,21 +44,27 @@ public:
     
     int posicao(T* umDado)
     {
+        Elemento<T>* dadoPosicao;
         dadoPosicao = ListaEncadeadaAbstrata<T>::_primeiro;
         for (int i = 0; i < getTamanho(); i++)
         {
             if(dadoPosicao->_dado == umDado)
             {
                 return i + 1;
+                dadoPosicao = NULL;
+                delete dadoPosicao;
             }
 
             dadoPosicao = dadoPosicao->_proximo;
         }
         return 0;
+        dadoPosicao = NULL;
+        delete dadoPosicao;
     }
     
     bool contem(T* umDado)
     {
+        Elemento<T>* dadoContem;
         dadoContem = ListaEncadeadaAbstrata<T>::_primeiro;
         for (int i = 0; i < getTamanho(); i++)
         {
@@ -79,19 +80,11 @@ public:
     
     void adicionaNoInicio(T* umDado)
     {
-        if(getTamanho() == 0)
-        {
-            novoDado_inicio->_proximo = ListaEncadeadaAbstrata<T>::_primeiro;
-            novoDado_inicio->_dado = umDado;
-            ListaEncadeadaAbstrata<T>::_primeiro = novoDado_inicio;
-        }else
-        {
-            novoDado_inicio++;
-            novoDado_inicio->_proximo = ListaEncadeadaAbstrata<T>::_primeiro;
-            novoDado_inicio->_dado = umDado;
-            ListaEncadeadaAbstrata<T>::_primeiro = novoDado_inicio;
-        }
-
+        Elemento<T>* Dado_inicio = new Elemento<T>;
+        
+        Dado_inicio->_proximo = ListaEncadeadaAbstrata<T>::_primeiro;
+        Dado_inicio->_dado = umDado;
+        ListaEncadeadaAbstrata<T>::_primeiro = Dado_inicio;
         
         ListaEncadeadaAbstrata<T>::_tamanho += 1;
     }
@@ -102,42 +95,43 @@ public:
         {
             throw(posicao_invalida_exception());
         }
-        dadoPosicao = ListaEncadeadaAbstrata<T>::_primeiro;
-        int j = 0;
-        while(j != umaPosicao - 1)
-        {
-                j++;
-                dadoAnterior->_proximo = dadoPosicao->_proximo;
-                dadoAnterior->_dado = dadoPosicao->_dado;
-                dadoPosicao = dadoPosicao->_proximo;
-        }
-        if((umaPosicao == 0) || getTamanho() == 0)
+        
+        if((umaPosicao == 1) || getTamanho() == 0)
         {
             adicionaNoInicio(umDado);
         }else{
-            dadoPosicao = ListaEncadeadaAbstrata<T>::_primeiro;
-            novoDado_inicio = ListaEncadeadaAbstrata<T>::_primeiro;
-            for (int i = 0; i <= getTamanho(); i++)
+                Elemento<T>* anteriores;
+                Elemento<T>* proximo;
+                Elemento<T>* atual;
+                Elemento<T>* dado = new Elemento<T>;
+
+                dado->_dado = umDado;
+                dado->_proximo = NULL;
+                atual = ListaEncadeadaAbstrata<T>::_primeiro;
+
+                int j = 1;
+
+            while(j != umaPosicao)
             {
-                if(i == umaPosicao - 1)
-                {
-                    dadoAnterior->_proximo = novoDado_posicao;
-                    novoDado_posicao->_proximo = dadoPosicao;
-                    novoDado_posicao->_dado = umDado;
-                    if(dadoAnterior->_dado != novoDado_inicio->_dado)
-                    {
-                        novoDado_inicio->_proximo = dadoAnterior;
-                        ListaEncadeadaAbstrata<T>::_primeiro = novoDado_inicio;
-                        break;
-                    }
-                    ListaEncadeadaAbstrata<T>::_primeiro = dadoAnterior;
-                    break;
-                }
-                dadoPosicao = dadoPosicao->_proximo;
+                anteriores = atual;
+                atual = atual->_proximo;
+                j++;
             }
+
+            if(atual == NULL)
+            {
+                dado->_proximo = atual;
+                anteriores->_proximo = dado;
+            }else
+            {
+                dado->_proximo = atual;
+                anteriores->_proximo = dado;
+            }  
+
             ListaEncadeadaAbstrata<T>::_tamanho += 1;
         }
     }
+
     void adicionaNoFim(T* umDado)
     {
         adicionaNaPosicao(umDado, getTamanho() + 1);
@@ -145,6 +139,9 @@ public:
 
     T* retiraDoInicio()
     {
+        Elemento<T>* novoDado_inicio;
+        Elemento<T>* novoDado_posicao;
+        T* dado;
         if(getTamanho() == 0)
         {
             throw lista_encadeada_vazia_exception();
@@ -155,11 +152,19 @@ public:
 
         ListaEncadeadaAbstrata<T>::_primeiro = novoDado_posicao;
         ListaEncadeadaAbstrata<T>::_tamanho -= 1;
-        return novoDado_inicio->_dado;
+        dado = novoDado_inicio->_dado;
+        delete novoDado_inicio;
+        return dado;
     }
 
     T* retiraDaPosicao(int umaPosicao)
     {
+        Elemento<T>* novoDado_inicio;
+        Elemento<T>* dadoAnterior;
+        Elemento<T>* novoDado_posicao;
+        Elemento<T>* dadoRetirar;
+        Elemento<T>* dadoProximo;
+        
         T* dado;
         if((umaPosicao <= 0) || (umaPosicao >= getTamanho() + 1))
         {
@@ -187,11 +192,11 @@ public:
             j--;            
         }
         dadoAnterior->_proximo = dadoProximo;
-        novoDado_inicio++;
         dado = dadoRetirar->_dado;
         ListaEncadeadaAbstrata<T>::_tamanho -= 1;
+        delete dadoRetirar;
         }
-
+        
         return dado;
     }
 
